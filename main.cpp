ce727aa681f96395013a010d91eacb20d8f3211c
LFRusso <lf.santos@usp.br>
2020-01-05 23:36:54 -0300

    testando rede Paumi

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ int main(){
 
     // Setting up quadtree from dataset
     Quadtree quadtree;
-    quadtree.read_from_table("table.dat"); // change input file name here 
+    quadtree.read_from_table("rede1"); // change input file name here 
     quadtree.start();
 
-    // // Rectangles output file
-    // std::ofstream data;
-    // data.open("rects.dat");
-    // quadtree.visualize(data);
-    // data.close();
+    // Rectangles output file
+    std::ofstream data;
+    data.open("rects.dat");
+    quadtree.visualize(data);
+    data.close();
 
     // Query for something in the quadtree
     quadtree.query(0); // 0 = points separation
--- view.py
+++ view.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import matplotlib.patches as patches
 
 x1, x2, y1, y2 = np.loadtxt("rects.dat", unpack=True)
-x, y = np.loadtxt("table.dat", unpack=True)
+x, y = np.loadtxt("rede1", unpack=True)
 
 fig,ax = plt.subplots(1)
 
